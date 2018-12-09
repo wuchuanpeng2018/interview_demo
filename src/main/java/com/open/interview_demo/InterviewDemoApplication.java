91157b187f48b257aea07bfb2b85fb2aa24d1415
wuchp <1217631035@qq.com>
2018-12-09 10:46:28 +0800

    提交maven项目

---
+++ src/main/java/com/open/interview_demo/InterviewDemoApplication.java
@@ -0,0 +1,12 @@
+package com.open.interview_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewDemoApplication.class, args);
+	}
+}
